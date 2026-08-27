35e57e6ec23af2cb0ee91748e26cc8059919c1b8
SahooVista <sahooamit338@gmail.com>
2026-08-27 19:05:28 +0530

    day6

--- RAG/prepare.js
+++ RAG/prepare.js
@@ -1,6 +1,26 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import {RecursiveCharacterTextSplitter} from "@langchain/textsplitters";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import {PineconeStore} from "@langchain/pinecone";
+import {Pinecone as PineconeClient} from "@pinecone-database/pinecone";
+import dotenv from "dotenv";
+dotenv.config();
+const embeddings= new GoogleGenerativeAIEmbeddings({
+        api:process.env.GOOGLE_API_KEY,
+        model:'text-embedding-004'
+    });
+const pinecone = new PineconeClient();
+const vectorStore = new PineconeStore(embeddings,{
+    pineconeIndex,
+    maxConcurrency:5,
+}) 
 export async function indexTheDocument(filePath){
     const loader = new PDFLoader(filePath);
     const docs = await loader.load();
-    console.log(docs);
+    const Split= new RecursiveCharacterTextSplitter({
+        chunkSize:1000,
+        chunkOverlap:200
+    });
+    const chunkedDOcs= await Split.splitText(docs)
+
 }
